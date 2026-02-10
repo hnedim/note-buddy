0c0123ece1e03800268a4d73cf436756165864f5
hnedim <nhodzic098@gmail.com>
2026-02-10 14:48:32 +0100

    Frontend React, Vite, Tailwind & DaisyUI setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:5001/api/notes")
-      .then((res) => res.json())
-      .then((data) => setNotes(data))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
-    <>
-      {notes.map((note) => {
-        return (
-          <div className="note">
-            <h2>{note.title}</h2>
-            <p>{note.content}</p>
-          </div>
-        );
-      })}
-    </>
+    <div data-theme="forest">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/create" element={<CreatePage />}></Route>
+        <Route path="/note/:id" element={<NoteDetailPage />}></Route>
+      </Routes>
+    </div>
   );
 }
 
